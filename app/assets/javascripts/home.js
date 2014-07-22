680a57644ccb205c929c61fa26ed638a30097656
arukoh <arukoh10@gmail.com>
2014-07-23 01:17:47 +0900

    add feature of saving as PNG

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,8 @@
 //= require modernizr.custom
 $(function() {
-  const MAX_IMAGE_SIZE = 256;
+//  const DOMURL = self.URL || self.webkitURL || self;
   const PREVIEW_SIZE = 128;
-
-  var imgWidth = MAX_IMAGE_SIZE;
-  var imgHeight = MAX_IMAGE_SIZE;
+  var previews = {};
   var zindex = 0;
 
   var noneCroping = $('input[name=croping][value=none]');
@@ -45,7 +43,7 @@ $(function() {
     e.preventDefault();
     selectBtn.click();
   });
-  
+
   function compareWidthHeight(width, height) {
     var diff = [0, 0];
     if (width > height) {
@@ -57,97 +55,133 @@ $(function() {
   }
 
   var processFiles = function(files) {
-    if (files && typeof FileReader !== "undefined") {
-      for(var i=0; i<files.length; i++) {
-        readFile(files[i]);
-      }
-    } else { }
-  }
-
-  var readFile = function(file) {
-    if ( (/image/i).test(file.type) ) {
-      var reader = new FileReader();
-      reader.onload = function(e) {
-        var image = $('<img/>').load(function() {
-          var newimageurl = getCanvasImage(this);
-          createPreview(file, newimageurl);
-        })
-        .attr('src', e.target.result);
-        image = null;
-      }
-      reader.readAsDataURL(file);
-
-      $('#err').text('');
-    } else {
-      $('#err').text('*Selected file format not supported!');
+    if (!files || typeof FileReader === "undefined") {
+      return;
     }
-  }
-
-  var getCanvasImage = function(image) {
+    var effect = $('input[name=effect]:checked').val();
     var croping = $('input[name=croping]:checked').val();
     var imageSize = $('input[name=image-size]').val();
 
-    var diff = [0, 0];
-    if (croping != 'none') {
-      var sizeArray = [image.width, image.height, parseInt(imageSize)].filter(Boolean);
-      imgWidth = imgHeight = Math.min.apply(null, sizeArray);
-      diff = compareWidthHeight(image.width, image.height);
-    } else {
-      var maxSize = Math.max.apply(null, [image.width, image.height]);
-      maxSize = (imageSize) ? Math.min.apply(null, [imageSize, maxSize]) : maxSize;
-      if (image.width > image.height) {
-        imgWidth = maxSize;
-        imgHeight = Math.round(maxSize * image.height / image.width);
+    for(var i=0; i<files.length; i++) {
+      var file = files[i];
+      if (file.type.match(/image.*/)) {
+        var id = Math.random().toString(36).slice(-8);
+        previews[id] = {
+          id:      id,
+          file:    file,
+          name:    file.name.substr(0, file.name.lastIndexOf('.')),
+          effect:  effect,
+          croping: croping,
+          size:    imageSize,
+        }
+        createPreview(previews[id]);
+        $('#err').text('');
       } else {
-        imgWidth = Math.round(maxSize * image.width / image.height);
-        imgHeight = maxSize;
+        $('#err').text('*Selected file format not supported!');
       }
     }
-
-    var canvas = document.createElement('canvas');
-    canvas.width = imgWidth;
-    canvas.height = imgHeight;
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(image, diff[0]/2, diff[1]/2, image.width-diff[0], image.height-diff[1], 0, 0, imgWidth, imgHeight);
-    return canvas.toDataURL("image/png");
   }
 
-  var createPreview = function(file, url) {
-    var undefined;
-    var imageObj = {};
-    imageObj.id = '';
-    imageObj.filePath = url;
-    imageObj.fileName = file.name.substr(0, file.name.lastIndexOf('.'));
-    imageObj.width = imgWidth;
-    imageObj.height = imgHeight;
-    imageObj.realWidth = imgWidth;
-    imageObj.realHeight = imgHeight;
-    imageObj.effect = $('input[name=effect]:checked').val();
-    imageObj.croping = $('input[name=croping]:checked').val();
+  var createPreview = function(preview) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var image = new Image();
+      image.onload = function() {
+        var dimm     = calcDimension(this.width, this.height, PREVIEW_SIZE, preview.croping != 'none');
+        var realDimm = calcDimension(this.width, this.height, preview.size, preview.croping != 'none');
+        var canvas = document.createElement('canvas');
+        drawImage(canvas, this, dimm);
 
-    var img = $("#image-template").tmpl(imageObj)
-    var wrapSVG = $('figure .wrap-svg', img);
-    var blob = new Blob([wrapSVG.html()], {'type':'image/svg+xml'});
-    var domURL = self.URL || self.webkitURL || self;
-    var url = domURL.createObjectURL(blob);
+        preview.image      = this;
+        preview.url        = canvas.toDataURL("image/png");
+        preview.dimm       = dimm;
+        preview.realDimm   = realDimm;
+        preview.width      = dimm.dw - dimm.dx;
+        preview.height     = dimm.dh - dimm.dy;
+        preview.realWidth  = realDimm.dw - realDimm.dx;
+        preview.realHeight = realDimm.dh - realDimm.dy;
+        showPreview(preview);
+        preview.url = null;
+      }
+      image.src = e.target.result;
+    }
+    reader.readAsDataURL(preview.file);
 
-    imageObj.id = Math.random().toString(36).slice(-8);
-    imageObj.width = Math.round(PREVIEW_SIZE * imgWidth / Math.max.apply(null, [imgWidth, imgHeight]));
-    imageObj.height = Math.round(PREVIEW_SIZE * imgHeight / Math.max.apply(null, [imgWidth, imgHeight]));
-    var randvalue = Math.floor(Math.random()*31)-15;
-    img = $("#image-template").tmpl(imageObj).prependTo("#result")
-    .hide()
-    .css({
-      'Transform': 'scale(1) rotate('+randvalue+'deg)',
-      'msTransform': 'scale(1) rotate('+randvalue+'deg)',
-      'MozTransform': 'scale(1) rotate('+randvalue+'deg)',
-      'webkitTransform': 'scale(1) rotate('+randvalue+'deg)',
-      'OTransform': 'scale(1) rotate('+randvalue+'deg)',
-      'z-index': zindex++,
-    })
-    .show();
-    $('figure a', img).attr('href', url);
-//    domURL.revokeObjectURL(url);
+    var showPreview = function(preview) {
+      var randvalue = Math.floor(Math.random()*31)-15;
+      var img = $("#preview-template").tmpl(preview);
+      img.find(".wrap-svg").append($("#svg-template").tmpl(preview));
+      img.prependTo("#result")
+      .hide()
+      .css({
+        'Transform': 'scale(1) rotate('+randvalue+'deg)',
+        'msTransform': 'scale(1) rotate('+randvalue+'deg)',
+        'MozTransform': 'scale(1) rotate('+randvalue+'deg)',
+        'webkitTransform': 'scale(1) rotate('+randvalue+'deg)',
+        'OTransform': 'scale(1) rotate('+randvalue+'deg)',
+        'z-index': zindex++,
+      })
+      .show();
+
+      img.find("#preview-btn").click(function() {
+        $("#preview-modal-image").attr("src", "");
+
+        var prev = $.extend(true, {}, preview);
+        var canvas = document.createElement('canvas');
+        drawImage(canvas, prev.image, prev.realDimm);
+        prev.id     = '';
+        prev.url    = canvas.toDataURL("image/png");
+        prev.width  = prev.realWidth;
+        prev.height = prev.realHeight;
+        var svg = $("<div>").html($("#svg-template").tmpl(prev)).html();
+//        var blob = new Blob([svg], {type: "image/svg+xml;charset=utf-8"});
+//        var svgUrl = DOMURL.createObjectURL(blob);
+        var svgUrl = 'data:image/svg+xml;base64,' + window.btoa(svg);
+
+        var img = new Image();
+        img.onload = function () {
+          var canvas = document.createElement('canvas');
+          canvas.width  = prev.width;
+          canvas.height = prev.height;
+          canvas.getContext("2d").drawImage(img, 0, 0);
+          var pngUrl = canvas.toDataURL("image/png");
+          $("#preview-modal-image").attr("src", pngUrl);
+
+          var fileName = prev.name+"_"+prev.effect+"_"+prev.croping+"_"+prev.realWidth+"x"+prev.realHeight;
+          $("#preview-modal-svg-link").attr("href", svgUrl);
+          $("#preview-modal-svg-link").attr("download", fileName + ".svg");
+          $("#preview-modal-png-link").attr("href", pngUrl);
+          $("#preview-modal-png-link").attr("download", fileName + ".png");
+        }
+        img.src = svgUrl;
+      });
+    }
+  }
+
+  var calcDimension = function(width, height, limit, croping) {
+    var w, h, diff = [0, 0];
+    if (croping) {
+      diff = compareWidthHeight(width, height);
+      w = h = Math.min.apply(null, [width, height, limit].filter(Boolean));
+    } else {
+      w = h = Math.min.apply(null, [limit, Math.max.apply(null, [width, height])].filter(Boolean));
+      if (width > height) {
+        h = Math.round(h * height / width);
+      } else {
+        w = Math.round(w * width / height);
+      }
+    }
+    return {
+      sx: diff[0]/2, sy: diff[1]/2, sw: width-diff[0], sh: height-diff[1],
+      dx: 0,         dy: 0,         dw: w,             dh: h,
+    };
+  }
+
+  var drawImage = function(canvas, image, dimm) {
+    canvas.width  = dimm.dw - dimm.dx;
+    canvas.height = dimm.dh - dimm.dy;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(image, dimm.sx, dimm.sy, dimm.sw, dimm.sh, dimm.dx, dimm.dy, dimm.dw, dimm.dh);
   }
 
   if (typeof FileReader === "undefined") {
